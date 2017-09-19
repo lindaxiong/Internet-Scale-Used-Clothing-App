1630a316175350edc6bfefd1f0452e78e64e90c6
Jack Perales <peralesjack@gmail.com>
2017-09-19 15:03:43 -0400

    Notes form Pinckney on Views file added

--- uc_app/v1/views.py
+++ uc_app/v1/views.py
@@ -7,6 +7,8 @@ from django.http import JsonResponse
 def homepage(request):
     return HttpResponse("Basic Homepage")
 
+#TODO: Error Checking - shouldn't be able to do a GET method when creating - SEE: DECORATORS!!
+#TODO: Return JSON with the USER ID so you can use it.
 def create_user(request):
     #Ensure it's a POST request - need info to populate fields.
     if request.method == "POST":
@@ -16,19 +18,28 @@ def create_user(request):
         if form.is_valid():
             #save the instance
             form.save()
+            # Return user ID JSON
 
+#TODO: Error Checking: ID not found, raise problem if form is NOT valid.
 def access_user(request, user_id):
     if request.method == "POST":
+        # Finds the specific instance of the user
+        #TODO: What if ID not found?
         user_instance = User.objects.get(pk=user_id)
+        #TODO: Change from resubmitting an entire form to editing single fields if necessary
+        #TODO: Add restrictions for changing certain fields (not *super* relevant right now)
         user = UserForm(request.POST, instance=user_instance)
+        #TODO: Add JSON response indicating success
         if user.is_valid():
             user.save()
     if request.method == "GET":
         user_instance = User.object.get(pk=user_id)
         #JSON Response requires a dictionary input
+        #TODO: Think about what fields you *specifically* want to return. Must be a dictionary.
         return JsonResponse(model_to_dict(user_instance))
 
 
+#TODO: Repeat above steps on the following - should be near-identitical
 def create_item(request):
     if request.method == "POST":
         form = ItemForm(request.POST)

43809a59179edd9293ea30ba297b31a42edf4582
Linda Xiong <rx5zv@virginia.edu>
2017-10-04 18:53:18 -0400

    added get_top_items exp api, need to handle errors gracefully

--- exp_app/exp/urls.py
+++ exp_app/exp/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
-from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    url(r'^item/page/info/(?P<item_id>[0-9a-z]+)/$', views.get_item_page_info, name='item-page-info'),
+    url(r'^items/(?P<item_id>[0-9a-z]+)/$', views.get_item_page_info, name='item-page-info'),
+    url(r'^items/', views.get_top_items, name='top-results')
 ]
--- exp_app/exp/views.py
+++ exp_app/exp/views.py
@@ -4,21 +4,45 @@ import urllib.request
 import urllib.parse
 import json
 
+
+def get_top_items(request, item_ids=(4,5,6)):
+    response = {'data': []}
+    for item_id in item_ids:
+        item_req = urllib.request.Request('http://models-api:8000/api/v1/item/get/' + str(item_id) + '/')
+        item_resp_json = urllib.request.urlopen(item_req).read().decode('utf-8')
+        item_resp = json.loads(item_resp_json)
+        user_id = item_resp['seller']
+        user_req = urllib.request.Request('http://models-api:8000/api/v1/user/get/' + str(user_id) + '/')
+        user_resp_json = urllib.request.urlopen(user_req).read().decode('utf-8')
+        user_resp = json.loads(user_resp_json)
+        result_resp = {'item_name':item_resp['item_name'],
+                        'item_price':item_resp['item_price'],
+                        'seller_username':user_resp['username'],
+                        'description':item_resp['description'],
+                        'image_url':item_resp['image_url'],
+                        'item_size':item_resp['item_size'],
+                        'item_type':item_resp['item_type']}
+        response['data'].append(result_resp)
+    return JsonResponse(response, status=200)
+
+
 def get_item_page_info(request, item_id=0):
-    item_req = urllib.request.Request('http://models-api:8000/api/v1/item/get/' + item_id + '/')
+    item_req = urllib.request.Request('http://models-api:8000/api/v1/item/get/' + str(item_id) + '/')
     item_resp_json = urllib.request.urlopen(item_req).read().decode('utf-8')
     item_resp = json.loads(item_resp_json)
     user_id = item_resp['seller']
     user_req = urllib.request.Request('http://models-api:8000/api/v1/user/get/' + str(user_id) + '/')
     user_resp_json = urllib.request.urlopen(user_req).read().decode('utf-8')
     user_resp = json.loads(user_resp_json)
-    return JsonResponse({'item_name':item_resp['item_name'],
+    response = {'data': []}
+    response['data'].append({'item_name':item_resp['item_name'],
     'item_price':item_resp['item_price'],
     'seller_username':user_resp['username'],
     'description':item_resp['description'],
     'image_url':item_resp['image_url'],
     'item_size':item_resp['item_size'],
     'item_type':item_resp['item_type']})
+    return JsonResponse(response, status=200)
 
 
 # Create your views here.

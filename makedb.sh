1de12fe75efe9f22d38c8433a99ac9424bd194d7
Linda <rx5zv@virginial.edu>
2017-10-22 13:16:42 -0400

    bash

---
+++ makedb.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+sleep 15
+
+if [[ -e /app/db/cs4501 ]]; then
+	    mysql -uroot -p'$3cureUS' -h db -Bse \
+		        "drop database if exists cs4501;
+	        create database cs4501 character set utf8;
+		    grant all on *.* to 'www'@'%'; \
+			        ";
+	    else
+		        mysql -uroot -p'$3cureUS' -h db -Bse \
+				    "drop user if exists 'www'@'%';
+			    flush privileges;
+			        drop database if exists cs4501;
+				    create user 'www'@'%' identified by '\$3cureUS';
+				        create database cs4501 character set utf8;
+					    grant all on *.* to 'www'@'%'; \
+						        ";
+				    fi
+
+				    echo "Successfully created 'cs4501' table with root user 'www'@'%'"
+
+				    find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+				    find . -path "*/migrations/*.pyc"  -delete
+
+				    echo "Successfully deleted old migrations"
